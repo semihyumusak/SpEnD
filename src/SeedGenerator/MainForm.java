dbc960cd81265fee985eedb2f88a77263ac38080
UYSAL\Elif <elifuysal23@gmail.com>
2017-03-07 12:33:26 +0300

    test commit

--- src/SeedGenerator/MainForm.java
+++ src/SeedGenerator/MainForm.java
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -2894,7 +2894,7 @@ public class MainForm extends JFrame implements ActionListener, WindowListener {
      }
      webClient.closeAllWindows();
      } catch (Exception ex) {
-
+System.out.println(ex);
      }
      }
      */
